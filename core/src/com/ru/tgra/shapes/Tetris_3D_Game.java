69397cee3872de9239c87bfcc6e9156b2d6ee719
g3n3r4tor <ingibergur90@gmail.com>
2015-10-30 16:23:56 +0000

    Kinda functioning game, rotation out of board will crash game though. Need to get a better camera angle as well

--- core/src/com/ru/tgra/shapes/Tetris_3D_Game.java
+++ core/src/com/ru/tgra/shapes/Tetris_3D_Game.java
@@ -20,12 +20,16 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 	private float test;
 	private float fov = 90.0f;
 	private int shape;
+	private int rotation = 0;
 	private boolean[][] board;
+	private float[][][] color;
+	private float[] shapeColor;
+	private float[][] position;
 	@Override
 	public void create () {
 		
 		DisplayMode disp = Gdx.graphics.getDesktopDisplayMode();
-		Gdx.graphics.setDisplayMode(disp.width, disp.height, true);
+		//Gdx.graphics.setDisplayMode(disp.width, disp.height, true);
 		
 		shader = new Shader();
 		
@@ -48,22 +52,22 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 
 		cam = new Camera();
 		cam.look(new Point3D(-3f, 0f, 3f), new Point3D(0,3,0), new Vector3D(0,1,0));
-		dropTime = 2;
+		dropTime = 1;
 		test = 0;
 		getNewShape();
-		//shape = 0;
-		Random rand = new Random();
-		board = new boolean[10][22];
-		/*for(int i = 0; i < 10; i++){
-			for(int j = 0; j <20; j++) {
-				if(rand.nextInt(2) == 0){
-					board[i][j] = true;
-				}
-				else {
-					board[i][j] = false;
-				}
+		//shape = 2;
+		board = new boolean[10][25];
+		position = new float[4][2];
+		color = new float[10][25 ][3];
+		shapeColor = new float[3];
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j <22; j++) {
+				color[i][j][0] = 1;
+				color[i][j][1] = 1;
+				color[i][j][2] = 1;
+				//board[i][j] = true;
 			}
-		}*/
+		}
 	}
 
 	private void input(float deltaTime)
@@ -80,27 +84,54 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
 			cam.pitch(-90.0f * deltaTime);
 		}
-		if(Gdx.input.isKeyJustPressed(Input.Keys.A) && ModelMatrix.main.getOrigin().x > -5) {
-			ModelMatrix.main.addTranslationBaseCoords(-1, 0, 0);
+		if(Gdx.input.isKeyJustPressed(Input.Keys.A)) {
+			boolean no = false;
+			for(int i = 0; i < 4; i++) {
+				if(position[i][0] <= -5) {
+					no = true;
+					break;
+				}
+			}
+			if(!no) {
+				if(!checkCollision()) {
+					ModelMatrix.main.addTranslationBaseCoords(-1, 0, 0);
+				}
+			}
 			//cam.slide(-3.0f * deltaTime, 0, 0);
 		}
-		if(Gdx.input.isKeyJustPressed(Input.Keys.D) && ModelMatrix.main.getOrigin().x < 4) {
-			ModelMatrix.main.addTranslationBaseCoords(1, 0, 0);
-
+		if(Gdx.input.isKeyJustPressed(Input.Keys.D) ) {
+			boolean no = false;
+			for(int i = 0; i < 4; i++) {
+				if(position[i][0] >= 4) {
+					no = true;
+					break;
+				}
+			}
+			if(!no) {
+				if(!checkCollision()) {
+					ModelMatrix.main.addTranslationBaseCoords(1, 0, 0);
+				}
+			}
 			//cam.slide(3.0f * deltaTime, 0, 0);
 		}
 		if(Gdx.input.isKeyJustPressed(Input.Keys.W) && shape != 0) {
 			ModelMatrix.main.addRotationZ(-90);
+			rotation= (rotation - 90) % 360;
+			//System.out.println(rotation);
 		}
 		if(Gdx.input.isKeyJustPressed(Input.Keys.S) && shape != 0) {
-			ModelMatrix.main.addRotationZ(90);	
+			ModelMatrix.main.addRotationZ(90);
+			rotation = (rotation + 90) % 360;
+			//System.out.println(rotation);
 		}
 		if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
 			Gdx.graphics.setDisplayMode(500,500,false);
 			Gdx.app.exit();
 		}
 		if(Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-			ModelMatrix.main.matrix.put(13, -20);
+			if(!checkCollision()) {
+				drop();
+			}
 		}
 	}
 	
@@ -119,7 +150,7 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.perspectiveProjection(fov, 2.0f, 0.1f, 100.0f);
+		cam.perspectiveProjection(fov , 2.0f, 0.1f, 100.0f);
 		shader.setViewMatrix(cam.getViewMatrix());
 		shader.setProjectionMatrix(cam.getProjectionMatrix());
 		shader.setEyePosition(cam.eye.x, cam.eye.y, cam.eye.z, 1.0f);
@@ -146,26 +177,35 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 		shapeOnScreen();
 		//shapeO();
 		if(test > dropTime && ModelMatrix.main.getOrigin().y > -20) {
-			drop();
-			checkCollision();
+			if(!checkCollision()) {
+				drop();
+			} else {
+				stickShapeOnBoard();
+				ModelMatrix.main.loadIdentityMatrix();
+				getNewShape();
+			}
 			test = 0;
 		}
-		if(ModelMatrix.main.getOrigin().y <= -20) {
-			ModelMatrix.main.loadIdentityMatrix();
-			getNewShape();
-			
+		for(int i = 0; i < 4; i++) {
+			if(position[i][1] <= -20) {
+				stickShapeOnBoard();
+				ModelMatrix.main.loadIdentityMatrix();
+				getNewShape();
+			}
 		}
-		System.out.println(ModelMatrix.main.getOrigin());
+		
+		
 		
 	}
 	public void fillupBoard() {
-		shader.setMaterialDiffuse(1, 1, 0, 1);
+		
 		for(int i = 0; i < 10; i++) {
-			for(int j = 0; j < 20; j++) {
+			for(int j = 0; j < 22; j++) {
 				if(board[i][j]) {
+					shader.setMaterialDiffuse(color[i][j][0], color[i][j][1], color[i][j][2], 1);
 					ModelMatrix.main.pushMatrix();
 					ModelMatrix.main.loadIdentityMatrix();
-					ModelMatrix.main.addTranslation((i-5), -j, 0);
+					ModelMatrix.main.addTranslation((i-5), j-22, 0);
 					shader.setModelMatrix(ModelMatrix.main.getMatrix());
 					BoxGraphic.drawSolidCube();
 					ModelMatrix.main.popMatrix();
@@ -200,145 +240,236 @@ public class Tetris_3D_Game extends ApplicationAdapter implements InputProcessor
 	}
 	public void drop() {
 		ModelMatrix.main.addTranslationBaseCoords(0,-1,0);
+		for(int i = 0; i < 4; i++){
+			//System.out.println("i:" + i + ", x: " + (int)position[i][0] + ", y:" + (int)position[i][1]);
+		}
 	}
-	public void checkCollision() {
-		
+	public boolean checkCollision() {
+		for(int i = 0; i < 4; i++) {
+			int x = (int)position[i][0];
+			int y = (int)position[i][1];
+			//System.out.println("x: " + (x+5) + " y:" + (y+22));
+			if(board[x+5][y+22]){
+				return true;
+			}
+		}
+		return false;
+	}
+	public void stickShapeOnBoard() {
+		for(int i = 0; i < 4; i++){
+			int x = (int)position[i][0];
+			int y = (int)position[i][1];
+			board[x+5][y+22] = true;
+			color[x+5][y+22] = shapeColor; 
+		}
 	}
 	public void getNewShape() {
 		Random rand = new Random();
 		shape = rand.nextInt(7);
+		rotation = 0;
 	}
 	
 	public void shapeO(){
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =1;
+		shapeColor[1] =1;
+		shapeColor[2] =0;
 		shader.setMaterialDiffuse(1, 1, 0, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(1, 0, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0,1,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeI() {
+
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =0.2f;
+		shapeColor[1] =0.2f;
+		shapeColor[2] =1;
 		shader.setMaterialDiffuse(0.2f, 0.2f, 1, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0, 1, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0,1,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0,-2,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeS() {
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =1;
+		shapeColor[1] =0.5f;
+		shapeColor[2] =0;
 		shader.setMaterialDiffuse(1, 0.5f, 0.0f, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(1, 0, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1,-1,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeZ() {
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =0;
+		shapeColor[1] =1;
+		shapeColor[2] =0;
 		shader.setMaterialDiffuse(0, 1.0f, 0.0f, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1, 0, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(1,-1,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(1,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeL() {
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =1;
+		shapeColor[1] =0;
+		shapeColor[2] =0;
 		shader.setMaterialDiffuse(1, 0.0f, 0.0f, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0, 1, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0,-2,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(1,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeJ() {
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =1;
+		shapeColor[1] =0.8f;
+		shapeColor[2] =0.9f;
 		shader.setMaterialDiffuse(1, 0.8f, 0.9f, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0, 1, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(0,-2,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
 	public void shapeT() {
 		ModelMatrix.main.pushMatrix();
+		shapeColor[0] =0.5f;
+		shapeColor[1] =0;
+		shapeColor[2] =0.5f;
 		shader.setMaterialDiffuse(0.5f, 0.0f, 0.5f, 1);
 		shader.setMaterialSpecular(1, 1, 1, 1);
 		shader.setMaterialShine(50);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[0][0] = ModelMatrix.main.getOrigin().x;
+		position[0][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1, 0, 0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
-		
+		position[1][0] = ModelMatrix.main.getOrigin().x;
+		position[1][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(2,0,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[2][0] = ModelMatrix.main.getOrigin().x;
+		position[2][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.addTranslation(-1,-1,0);
 		shader.setModelMatrix(ModelMatrix.main.getMatrix());
+		position[3][0] = ModelMatrix.main.getOrigin().x;
+		position[3][1] = ModelMatrix.main.getOrigin().y;
 		BoxGraphic.drawSolidCube();
 		ModelMatrix.main.popMatrix();
 	}
